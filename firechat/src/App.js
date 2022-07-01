b156897f5a235bbea6c7dbd30e9068170ca19ddd
Pratika <pratikatripath1221@gmail.com>
2022-07-02 02:04:06 +0530

    Environment variable added and created Home.js

--- firechat/src/App.js
+++ firechat/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-      Hello World
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
