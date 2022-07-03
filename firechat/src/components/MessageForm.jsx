9c6890924797a5b832df69c8ac0405157615bf99
Pratika <pratikatripath1221@gmail.com>
2022-07-03 20:50:53 +0530

    Basic Structure to send text or image is created

--- firechat/src/components/MessageForm.jsx
+++ firechat/src/components/MessageForm.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Attachment from './svg/Attachment'
 
 export default function MessageForm() {
   return (
-    <div>
-      Message Form
-    </div>
+    <form action="" className='message_form'>
+        <label htmlFor="img">
+            <Attachment/>
+        </label>
+        <input type="file" accept='image/' id="img" style={{display:"none"}}/>
+        <div>
+            <input type="text" placeholder="Enter Message" id="" />
+        </div>
+        <div>
+            <button className='btn'>Send</button>
+        </div>
+    </form>
   )
 }
